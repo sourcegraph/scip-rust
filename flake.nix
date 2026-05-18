9042576dc3dff5714cbaae9d236aa536d5128bc5
jupblb <michal.kielbowicz@sourcegraph.com>
2026-05-18 15:29:40 +0200

    Add flake.nix for Nix packaging and Docker image
    - packages.scip-rust: writeShellApplication wrapping the scip-rust script
      with rust-analyzer, cargo, and rustc on PATH via runtimeInputs.
    - packages.docker (Linux only): minimal layered image built with
      dockerTools.buildLayeredImage; bundles scip-rust, CA certs, and git.
    - devShells.default: rust-analyzer, rustc, cargo.
    - checks.shellcheck: runs shellcheck against the wrapper script.

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        scip-rust = pkgs.writeShellApplication {
+          name = "scip-rust";
+          runtimeInputs = with pkgs; [
+            rust-analyzer
+            cargo
+            rustc
+          ];
+          text = builtins.readFile ./scip-rust;
+        };
+      in
+      {
+        packages = {
+          inherit scip-rust;
+          default = scip-rust;
+        }
+        // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+          docker = pkgs.dockerTools.buildLayeredImage {
+            name = "scip-rust";
+            tag = "latest";
+            contents = [
+              scip-rust
+              pkgs.dockerTools.caCertificates
+              pkgs.git
+            ];
+            fakeRootCommands = "mkdir -p /work /tmp";
+            enableFakechroot = true;
+            config = {
+              Cmd = [ "scip-rust" ];
+              WorkingDir = "/work";
+              Env = [ "HOME=/tmp" ];
+            };
+          };
+        };
+
+        checks.shellcheck = pkgs.runCommand "check-shellcheck" { } ''
+          ${pkgs.shellcheck}/bin/shellcheck ${./scip-rust}
+          touch $out
+        '';
+
+        devShells.default = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            rust-analyzer
+            rustc
+            cargo
+          ];
+        };
+      }
+    );
+}
